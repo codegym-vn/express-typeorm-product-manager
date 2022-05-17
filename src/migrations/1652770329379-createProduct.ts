d8c3d10eb5ff357dac8d949f46964840c80a931f
hungnv0902 <hungnv0902@gmail.com>
2022-05-17 14:27:31 +0700

    Add file

---
+++ src/migrations/1652770329379-createProduct.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class createProduct1652770329379 implements MigrationInterface {
+    name = 'createProduct1652770329379'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`product\` (\`id\` int NOT NULL AUTO_INCREMENT, \`price\` int NOT NULL, \`name\` varchar(255) NOT NULL, \`author\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`product\``);
+    }
+
+}
